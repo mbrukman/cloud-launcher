d7f9eea94606c7408e0a7eafea315251977e9252
Misha Brukman <mbrukman@google.com>
2014-08-01 23:50:27 -0400

    Added a test to verify that our code, tests, and data have no tabs.

---
+++ tests/no_tabs_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -u
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+#
+# Tests that there are no tabs in our source code or test data files.
+
+if grep '\t' *.{py,sh} testdata/*.{json,py,yaml} ../*.py; then
+  echo "ERROR: tabs found." >&2
+  exit 1
+fi
