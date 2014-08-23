5c491b6d6fe5b51e90966b119dbde7862418d372
Misha Brukman <mbrukman@google.com>
2014-08-23 17:56:38 -0400

    Removed the implicit default config of `ambari.yaml`.
    This is problematic because a user might not be expecting to bring up an Ambari
    cluster as part of running the tool, and also, Ambari config requires running
    `make` to generate the startup scripts first, so this avoids confusing errors.
    
    From now on, users will have to explicitly specify which config they want to
    run, which will make even more sense as we build up a library of configs and
    having it default to Ambari will become even more perplexing.

--- run.sh
+++ run.sh
@@ -20,10 +20,6 @@
 
 # The file containing settings such as project name, region, zone, etc.
 declare -r SETTINGS="${SETTINGS:-$(dirname $0)/settings.sh}"
-
-# The deployment configuration.
-declare -r CONFIG="${CONFIG:-apps/hortonworks/hdp2/centos6/ambari.yaml}"
-
 source "${SETTINGS}" || exit 1
 
-$(dirname $0)/run.py --project="${PROJECT}" --zone="${ZONE}" --config="${CONFIG}" "$@"
+$(dirname $0)/run.py --project="${PROJECT}" --zone="${ZONE}" "$@"
