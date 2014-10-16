52cab631eb69952f1d717fa16e02077f4633854b
Misha Brukman <mbrukman@google.com>
2014-10-17 10:13:05 +1100

    Updates for Google Python API 1.2 -> 1.3 upgrade.
    The change is: `apiclient` -> `googleapiclient`.

--- src/cloud_launcher.py
+++ src/cloud_launcher.py
@@ -27,9 +27,9 @@ import os
 import sys
 
 # Google Cloud API imports
-from apiclient.discovery import build
-from apiclient import errors
-from apiclient import http
+from googleapiclient.discovery import build
+from googleapiclient import errors
+from googleapiclient import http
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client import gce
